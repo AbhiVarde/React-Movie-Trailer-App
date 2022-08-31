a846bb600bedf50a6878572e36de00950700a34d
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2022-08-31 11:18:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+import movieTrailer from 'movie-trailer';
+
+function App() {
+
+  const [video, setVideo] = useState("Panchayat");
+  const [videoURL, setVideoURL] = useState("https://youtu.be/mojZJ7oeD_g");
+
+  function handleSearch() {
+    movieTrailer(video).then((res) => {
+      setVideoURL(res);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className='search'>
+        <label>
+          Search for any movies/shows : { " " }
+        </label>
+        <input type="text" 
+          onChange={(e) => { setVideo(e.target.value) }}>
+        </input>
+        &nbsp; &nbsp; &nbsp; 
+        <button onClick={() => {handleSearch()}}>
+          Search 
+        </button>
+      </div>
+      <ReactPlayer url={videoURL} controls={true} />
+    </div>
+  );
+}
+
+export default App;
